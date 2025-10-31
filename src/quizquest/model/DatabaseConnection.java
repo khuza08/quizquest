c1d26ab44cd2b6ef1e814f67a2af47556554d55d
elza <huzaifahasif3@gmail.com>
2025-10-31 15:48:45 +0700

    chore: change database connection

--- src/quizquest/model/DatabaseConnection.java
+++ src/quizquest/model/DatabaseConnection.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     // Ganti USER dan PASSWORD sesuai MySQL-mu
     private static final String URL = "jdbc:mysql://localhost:3306/quizquest?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USER = "root"; // ← GANTI JIKA PERLU
-    private static final String PASSWORD = "Asifppnk08"; // ← GANTI JIKA ADA PASSWORD
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
 
     static {
         try {
